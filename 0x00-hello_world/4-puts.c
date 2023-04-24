e166d44d98f25b805ccccefbb4846c34890512eb
tracy45 <njeritracy10@gmail.com>
2023-04-23 20:02:40 -0700

    Hello World, C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Retrun: Always 0 (Success)
+ */
+int main(){
+
+	puts("Programming is like building a multilingual puzzle,");
+
+	puts("");
+	
+	return 0;
+}
