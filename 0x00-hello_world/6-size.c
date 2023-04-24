a359544e9c121ac9aaedc71cd5c212fe1cc78c31
tracy45 <njeritracy10@gmail.com>
2023-04-23 20:28:34 -0700

    Hotfix task 4 & 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -8,13 +7,10 @@
 int main(void) 
 {
     printf("Size of char: %zu byte(s)\n", sizeof(char));
-    printf("Size of short: %zu byte(s)\n", sizeof(short));
     printf("Size of int: %zu byte(s)\n", sizeof(int));
-    printf("Size of long: %zu byte(s)\n", sizeof(long));
-    printf("Size of long long: %zu byte(s)\n", sizeof(long long));
+    printf("Size of long: %zu byte(s)\n", sizeof(long int));
+    printf("Size of long long: %zu byte(s)\n", sizeof(long long int));
     printf("Size of float: %zu byte(s)\n", sizeof(float));
-    
-
     return (0);
 }
 
